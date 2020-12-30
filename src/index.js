898189084937f0bdbc3e47cbfcaeb570c820011b
avbutlov <butloff666@gmail.com>
2020-12-30 05:09:40 +0300

    feat: api-client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-console.log('ddd')
+class SwapiService {
+
+  constructor() {
+    this._apiBase = 'http://swapi.dev/api';
+  }
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if(!res.ok) {
+      throw new Error(`Could not fetch ${url}`);
+    }
+    const body = await res.json();
+    return body
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`)
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`)
+  }
+  
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllStarships().then((people) => {
+    console.log(people);
+  
+})
