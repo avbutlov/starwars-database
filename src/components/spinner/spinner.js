e5f37ec9b4110ff7347ab665a82ad6e76ef2f765
avbutlov <butloff666@gmail.com>
2021-01-14 04:25:01 +0300

    feat: loading spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-u97m2yz364">
+      <div className="ldio-zq85abux4ml">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
